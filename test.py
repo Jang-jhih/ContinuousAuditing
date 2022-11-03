11e9056076023a3d885320065db8b598329dd827
jang-jhih <zebraking30@gmail.com>
2022-11-04 00:09:19 +0800

    test2

---
+++ test.py
@@ -0,0 +1,1 @@
+x=2if x == 1:    print(1)    if x == 2:        print(2)        if x == 3:            print(3)            if x == 4:                print(4)                if x == 5:                    print(5)
